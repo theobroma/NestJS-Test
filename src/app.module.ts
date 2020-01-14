d2a275213721763012bf2580fa57e8c5c4ebef79
Alexandr Siryj <theobroma222@gmail.com>
2020-01-14 14:14:09 +0200

    dogs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()), DogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -38,7 +38,7 @@ class ConfigService {
       password: this.getValue('POSTGRES_PASSWORD'),
       database: this.getValue('POSTGRES_DATABASE'),
 
-      entities: ['src/entity/**/*.entity{.ts,.js}'],
+      entities: ['**/*.entity{.ts,.js}'],
 
       migrationsTableName: 'migration',
 
